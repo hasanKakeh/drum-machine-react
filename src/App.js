ce30a6d7532d55ba969cc3063c01f8f8da5e1e09
HasanKakeh <hasan.kakeh1@gmail.com>
2021-05-03 02:46:37 +0300

    update

--- src/App.js
+++ src/App.js
@@ -3,22 +3,26 @@ import "bootstrap/dist/css/bootstrap.css";
 import { Component } from "react";
 class App extends Component {
   state = {
+    text: "",
     drum: [
       [
         {
           id: "Q",
           src:
             "https://sampleswap.org/samples-ghost/DRUMS%20(FULL%20KITS)/DRUM%20MACHINES/Linn%20Drums/34[kb]linn-BDRUM13.wav.mp3",
+          desc: "dsadsa",
         },
         {
           id: "W",
           src:
             "https://sampleswap.org/samples-ghost/DRUMS%20(FULL%20KITS)/DRUM%20MACHINES/Linn%20Drums/16[kb]linn-BONGO1.wav.mp3",
+          desc: "sdadsa",
         },
         {
           id: "E",
           src:
             "https://sampleswap.org/samples-ghost/DRUMS%20(FULL%20KITS)/DRUM%20MACHINES/Linn%20Drums/11[kb]linn-CABASA-v2.wav.mp3",
+          desc: "fdsfds",
         },
       ],
       [
@@ -26,16 +30,19 @@ class App extends Component {
           id: "A",
           src:
             "https://sampleswap.org/samples-ghost/DRUMS%20(FULL%20KITS)/DRUM%20MACHINES/Linn%20Drums/13[kb]linn-CHH.wav.mp3",
+          desc: "sdfdsf",
         },
         {
           id: "S",
           src:
             "https://sampleswap.org/samples-ghost/DRUMS%20(FULL%20KITS)/DRUM%20MACHINES/606%20Basic/40[kb]606-snare2.wav.mp3",
+          desc: "sdfge",
         },
         {
           id: "D",
           src:
             "https://sampleswap.org/samples-ghost/DRUMS%20(FULL%20KITS)/DRUM%20MACHINES/Linn%20Drums/15[kb]linn-Clap-1.wav.mp3",
+          desc: "sdfdsf",
         },
       ],
       [
@@ -43,16 +50,19 @@ class App extends Component {
           id: "Z",
           src:
             "https://sampleswap.org/samples-ghost/DRUMS%20(FULL%20KITS)/DRUM%20MACHINES/Linn%20Drums/30[kb]linn-CONGA-H-v2.wav.mp3",
+          desc: "sdfdsf",
         },
         {
           id: "X",
           src:
             "https://sampleswap.org/samples-ghost/DRUMS%20(FULL%20KITS)/DRUM%20MACHINES/606%20Basic/79[kb]606-tom1.wav.mp3",
+          desc: "sdfdsfs",
         },
         {
           id: "C",
           src:
             "https://sampleswap.org/samples-ghost/DRUMS%20(FULL%20KITS)/DRUM%20MACHINES/606%20Basic/54[kb]606-cymbal5.wav.mp3",
+          desc: "dfsdsf",
         },
       ],
     ],
@@ -60,9 +70,25 @@ class App extends Component {
     bank: false,
     volume: 50,
   };
-  handleClick(src) {
-    const audio = new Audio(src);
+  handleClick(item) {
+    var audio = document.getElementById(item.id);
     audio.play();
+    this.setState({ text: item.id });
+  }
+  onKeyPress(e) {
+    const key = e.key.toLowerCase();
+    var audio;
+    console.log(e);
+    if (key === "q") audio = document.getElementById("Q");
+    if (key === "e") audio = document.getElementById("E");
+    if (key === "c") audio = document.getElementById("C");
+    if (key === "s") audio = document.getElementById("S");
+    if (key === "w") audio = document.getElementById("W");
+    if (key === "a") audio = document.getElementById("A");
+    if (key === "d") audio = document.getElementById("D");
+    if (key === "z") audio = document.getElementById("Z");
+    if (key === "x") audio = document.getElementById("X");
+    if (audio) audio.play();
   }
   render() {
     return (
@@ -71,19 +97,26 @@ class App extends Component {
         className="App container mt-5 d-flex align-items-center justify-content-center"
         style={{ height: "100%" }}
       >
-        <div id="display" className="m-5 " style={{ width: 200, height: 200 }}>
+        <div className="m-5 " style={{ width: 200, height: 200 }}>
           {this.state.drum.map((row) => {
             return (
               <div className="row">
                 {row.map((item) => {
                   return (
                     <button
-                      id={item.id}
-                      class="btn btn-primary border col-4 h-200"
+                      id={item.src}
+                      className="btn drum-pad btn-primary border col-4 h-200"
                       style={{ height: 60 }}
-                      onClick={() => this.handleClick(item.src)}
+                      onClick={() => this.handleClick(item)}
+                      onKeyDown={(e) => this.onKeyPress(e)}
                     >
-                      {item.id}
+                      <audio
+                        id={item.id}
+                        preload="auto"
+                        className="clip"
+                        src={item.src}
+                      ></audio>
+                      <p onClick={() => this.handleClick(item)}>{item.id}</p>
                     </button>
                   );
                 })}
@@ -91,6 +124,7 @@ class App extends Component {
             );
           })}
         </div>
+        <p id="display">{this.state.text}</p>
       </div>
     );
   }
